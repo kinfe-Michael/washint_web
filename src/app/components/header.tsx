2a09ee9564d3833c717c026a01f3e24c1087e6ed
kinfe-Michael <kmichael3d@gmail.com>
2025-06-14 06:14:06 -0700

    main layout setup

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <header className="fixed top-0 left-0 right-0 z-50 bg-black text-white p-4 shadow-md">
+          
+        </header>
+  )
+}
+
+export default Header
---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+        <aside className="fixed hidden border lg:flex left-0 top-16 bottom-16 w-64 bg-black p-4 overflow-y-auto">
+            
+          </aside>
+  )
+}
+
+export default Sidebar
