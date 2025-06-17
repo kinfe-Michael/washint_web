613390edd7ae8a15daf2d37a727dbe55d74a3441
kinfe-Michael <kmichael3d@gmail.com>
2025-06-16 22:22:32 -0700

    scrollable component and muscic card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import BottomContainer from "./components/BottomContainer";
+import MusicCard from "./components/MusicCard";
+import Scroller from "./components/Scroller";
 import Sidebar from "./components/sidebar";
-
 
 export default function Home() {
   return (
     <div className="">
-      <Sidebar/>
-      <BottomContainer/>
-   
+      <Sidebar />
+      <BottomContainer />
+      <div className="m-4 ml-2 mt-20 lg:ml-64">
+        <Scroller title="favorite" >
+          <MusicCard url="/yohana.jpg" alt="yohana" title="Yetalesh" artist="Yohana"/>
+        </Scroller>
+      </div>
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function capitalizeFirstLetter(str:string) {
+  if (typeof str !== 'string' || str.length === 0) {
+    return ""; 
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
