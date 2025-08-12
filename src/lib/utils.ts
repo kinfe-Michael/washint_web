b2432dde704f592f1db57057c8ee7717c7edee2e
kinfe-michael <kmichael3d@gmail.com>
2025-08-12 21:22:39 +0300

    login with jwt and a middleware to protect routes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import Cookies from 'js-cookie';
+import axios from 'axios'
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,3 +13,16 @@ export function capitalizeFirstLetter(str:string) {
   }
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export const api = axios.create({
+  baseURL: 'http://localhost:8000/api/'
+})
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('access_token')
+    if(token){
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  }
+)
