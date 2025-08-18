ca1dfc95f457095648affe9288629fbe08cfbcb2
kinfe-michael <kmichael3d@gmail.com>
2025-08-18 10:09:30 +0300

    artist page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,11 @@
+import NavLink from "../components/CustomNavLink"
 import PageWraper from "../components/PageWraper"
 
 function page() {
   return (
     <PageWraper>
         <div>settings</div>
+        <NavLink href="/settings/artistDashbord">Artist Dashbord</NavLink>
     </PageWraper>
   )
 }
